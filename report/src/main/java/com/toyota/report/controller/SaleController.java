8610b9fd1d8c3c9ceafb14eed133fc1865b27b0f
berkaykopuz <berkaykopuz@hotmail.com>
2024-02-09 15:38:44 +0300

    add paging and filtering

--- report/src/main/java/com/toyota/report/controller/SaleController.java
+++ report/src/main/java/com/toyota/report/controller/SaleController.java
@@ -2,6 +2,7 @@ package com.toyota.report.controller;
 
 import com.toyota.report.entity.Sale;
 import com.toyota.report.service.SaleListingService;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,5 +28,11 @@ public class SaleController {
                                                        @RequestParam int month,
                                                        @RequestParam int day){
         return new ResponseEntity<>(saleListingService.findBySaleDate(year, month, day), HttpStatus.OK);
+    }
+
+    @GetMapping("paged")
+    public ResponseEntity<Page<Sale>> getAllPaged(@RequestParam(value="pageNo",defaultValue = "0",required = false) int pageNo,
+                                                  @RequestParam(value="pageSize",defaultValue = "1",required = false) int pageSize){
+        return new ResponseEntity<>(saleListingService.getAllSales(pageNo, pageSize),HttpStatus.OK);
     }
 }
--- report/src/main/java/com/toyota/report/service/SaleListingService.java
+++ report/src/main/java/com/toyota/report/service/SaleListingService.java
@@ -1,9 +1,13 @@
 package com.toyota.report.service;
 
+import com.toyota.report.entity.Product;
 import com.toyota.report.entity.ProductSale;
 import com.toyota.report.entity.Sale;
 import com.toyota.report.repository.ProductSaleRepository;
 import com.toyota.report.repository.SaleRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -23,5 +27,13 @@ public class SaleListingService {
 
     public List<Sale> findBySaleDate(int year, int month, int day){
         return saleRepository.findBySaleDate(year, month, day);
+    }
+
+    public Page<Sale> getAllSales(int pageNo, int pageSize){
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+
+        Page<Sale> salePage = saleRepository.findAll(pageable);
+
+        return salePage;
     }
 }
